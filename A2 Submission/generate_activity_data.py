fix average precision and recall when sum is 0

A2 Submission/generate_activity_data.py
@@ -7,19 +7,19 @@ Created on Wed Sep 14 10:40:38 2016
 Assignment A2 : Activity Recognition
 Part 1 : Supervised Classification
 
-This script generates noisy average and maximum speed data for 
-walking, running and biking and plots the datapoints along with 
-the decision boundary of a trained Suport Vector Machine (SVM) 
+This script generates noisy average and maximum speed data for
+walking, running and biking and plots the datapoints along with
+the decision boundary of a trained Suport Vector Machine (SVM)
 classifier.
 
-Note that this data is generated using means and standard deviations that 
-are not entirely realistic. When you collect actual data, you will likely 
-find that it is not as clean, i.e. there is more class overlap and therefore 
+Note that this data is generated using means and standard deviations that
+are not entirely realistic. When you collect actual data, you will likely
+find that it is not as clean, i.e. there is more class overlap and therefore
 more ambiguity.
 
-Run this script to visualize the generated data, each data point colored 
-according to the corresponding activity. Change the variable n_samples 
-to be much smaller (~10 samples) and much larger (> 1000 samples) and 
+Run this script to visualize the generated data, each data point colored
+according to the corresponding activity. Change the variable n_samples
+to be much smaller (~10 samples) and much larger (> 1000 samples) and
 see how the decision boundaries change.
 
 Refer to the assignment details on what you need to do here.
@@ -32,6 +32,7 @@ Refer to the assignment details on what you need to do here.
 #
 # -----------------------------------------------------------------------------
 
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -48,9 +49,9 @@ from sklearn import cross_validation
 n_samples = 100 # number of data points per activity
 n_dim = 2 # number of feature dimensions; 2 for this example so that we can visualize
 
-# for each activity, we specify the expected max, expected average and the standard deviation  
+# for each activity, we specify the expected max, expected average and the standard deviation
 # for simplicity the stddev is the same for both the max and average features.
-stats = {'walking' : {'expected_max' : 7.0, 'expected_average' : 3.1, 'stddev' : 1.25, 'samples' : [], 'format' : 'ro'}, 
+stats = {'walking' : {'expected_max' : 7.0, 'expected_average' : 3.1, 'stddev' : 1.25, 'samples' : [], 'format' : 'ro'},
          'running' : {'expected_max' : 10.5, 'expected_average' : 5.25, 'stddev' : 2.5, 'samples' : [], 'format' : 'go'},
          'biking' : {'expected_max' : 17.0, 'expected_average' : 9.25, 'stddev' : 16.0, 'samples' : [], 'format' : 'bo'}}
 
@@ -86,37 +87,42 @@ for index, (activity, values) in enumerate(stats.items()):
 
 n = len(y)
 
-# SVM regularization parameter : For the purpose of this assignment, we will 
-# just set it to 1.0; in real applications, you should choose the C that 
-# gives the best performance metrics on a validation dataset, a separate 
+# SVM regularization parameter : For the purpose of this assignment, we will
+# just set it to 1.0; in real applications, you should choose the C that
+# gives the best performance metrics on a validation dataset, a separate
 # dataset in addition to training/test. You don't need to do this.
-C = 1.0  
+C = 1.0
 
 clf = svm.SVC(kernel = 'linear', C=C )
 
 cv = cross_validation.KFold(n, n_folds=10, shuffle=False, random_state=None)
 
-def precision(conf, label):
+def get_precision(conf, label, instance_count):
     if (np.sum(conf[:, label]) == 0):
-        return 0.0
+        if instance_count[label] == 0:
+            return None
+        else:
+            return 0.0
     else:
         return conf[label][label] / np.sum(conf[:, label])
-    
-def recall(conf, label):
+
+def get_recall(conf, label, instance_count):
     if (np.sum(conf[label, :]) == 0):
-        return 0.0 
-    else: 
+        if instance_count[label] == 0:
+            return None
+        else:
+            return 0.0
+    else:
         return conf[label][label] / np.sum(conf[label, :])
-    
+
+label_to_name = {
+    0: 'Walking',
+    1: 'Biking',
+    2: 'Running'
+}
 accuracies = []
-
-precision_walking_lst = []
-precision_biking_lst = []
-precision_running_lst = []
-
-recall_walking_lst = []
-recall_biking_lst = []
-recall_running_lst = []
+precisions = [[], [], []]
+recalls = [[], [], []]
 
 for i, (train_indexes, test_indexes) in enumerate(cv):
     X_train = X[train_indexes, :]
@@ -124,70 +130,45 @@ for i, (train_indexes, test_indexes) in enumerate(cv):
     X_test = X[test_indexes, :]
     y_test = y[test_indexes]
     clf.fit(X_train, y_train)
-    
+
     # predict the labels on the test data
     y_pred = clf.predict(X_test)
-    
 
     # show the comparison between the predicted and ground-truth labels
     conf = confusion_matrix(y_test, y_pred, labels=[0,1,2])
-    
+
     print("Fold {} : The confusion matrix is :".format(i))
     print (conf)
-    
-    # TODO: Compute the accuracy, precision and recall from the confusion matrix
+
+    instance_count = [0, 0, 0]
+    for instance in y_test:
+        instance_count[instance] += 1
+
     accuracy = accuracy_score(y_test,y_pred)
-    
-    precision_walking = precision(conf, 0)
-    precision_biking = precision(conf, 1)
-    precision_running = precision(conf, 2)
-    
-    recall_walking = recall(conf, 0)
-    recall_biking = recall(conf, 1)
-    recall_running = recall(conf, 2)
-    
-    print("Accuracy: " + str(accuracy_score(y_test, y_pred)))
-    
-    print("Precision: ")
-    print("\tWalking: " + str(precision_walking))
-    print("\tBiking: " + str(precision_biking))
-    print("\tRunning: " + str(precision_running))
-    
-        
-    print("Recall: ")
-    print("\tWalking: " + str(recall_walking))
-    print("\tBiking: " + str(recall_biking))
-    print("\tRunning: " + str(recall_running))
-    
-    print("\n")
-    
     accuracies.append(accuracy)
-    
-    precision_walking_lst.append(precision_walking)
-    precision_biking_lst.append(precision_biking)
-    precision_running_lst.append(precision_running)
-    
-    recall_walking_lst.append(recall_walking)
-    recall_biking_lst.append(recall_biking)
-    recall_running_lst.append(recall_running)
-    
+    print("Accuracy: " + str(accuracy_score(y_test, y_pred)))
 
-# TODO: Output the average accuracy, precision and recall over the 10 folds
-print("AVERAGES: ")
-print("Accuracy: " + str(np.sum(accuracies)/len(accuracies)))
-print("Precision: ")
-print("\tWalking: " + str(np.sum(precision_walking_lst)/len(precision_walking_lst)))
-print("\tBiking: " + str(np.sum(precision_biking_lst)/len(precision_biking_lst)))
-print("\tRunning: " + str(np.sum(precision_running_lst)/len(precision_running_lst)))
-print("Recall: ")
-print("\tWalking: " + str(np.sum(recall_running_lst)/len(recall_running_lst)))
-print("\tBiking: " + str(np.sum(recall_biking_lst)/len(recall_biking_lst)))
-print("\tRunning: " + str(np.sum(recall_running_lst)/len(recall_running_lst)))
+    for label in [0, 1, 2]:
+        precision = get_precision(conf, label, instance_count)
+        recall = get_recall(conf, label, instance_count)
+
+        if precision != None:
+            precisions[label].append(precision)
+        if recall != None:
+            recalls[label].append(recall)
+
+        print(label_to_name[label])
+        print("\tPrecision: "  + str(precision))
+        print("\tRecall: "  + str(recall))
+
+    print("\n")
+
+print("Average Accuracy: " + str(np.sum(accuracies)/len(accuracies)))
+for label in [0, 1, 2]:
+    print(label_to_name[label])
+    print("\tAverage Precision: " + str(np.sum(precisions[label])/len(precisions[label])))
+    print("\tAverage Recall: "  + str(np.sum(recalls[label])/len(recalls[label])))
 print("\n")
-
-        
-# TOO: Then change the CV parameter shuffle to True and describe how the results change.
-
 
 # Train on entire dataset; that will give us the decision boundary we'll plot.
 clf.fit(X, y)